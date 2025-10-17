f5e8ca316a7ebb2eb06d071da1f4e0e43b864cfd
davithekid <byjdrk.27@gmail.com>
2025-10-17 08:59:00 -0300

    style layout

--- front-end/app/src/app/layout.jsx
+++ front-end/app/src/app/layout.jsx
@@ -20,6 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
+      <head>
+        <link rel="icon" href="/zelos.svg" sizes="any" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
